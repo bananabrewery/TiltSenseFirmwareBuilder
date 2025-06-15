9222c6192e23a3efd13a8f86c42bda6c57d1f5a7
Banana Brewery <215856280+bananabrewery@users.noreply.github.com>
2025-06-15 11:15:28 +0200

    Dynamic YAML Generation
    Continue building dynamic YAML blocks for ESPHome.

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,10 @@ import {
 import {IconInfoCircle, IconRadar, IconTestPipe2Filled} from '@tabler/icons-react';
 import {CodeHighlight} from "@mantine/code-highlight";
 import yaml from "js-yaml";
-import {TiltColorId, type TiltColorKey, TiltColors, TiltColorsHex, type Tilts,} from './models/Tilt';
+import {type Tilt, TiltColorId, type TiltColorKey, TiltColors, TiltColorsHex, type Tilts} from './models/Tilt';
 import tiltSenseTemplateYaml from './assets/tiltsense-template.yaml?raw'
 import tiltSenseFirmwareTemplate from './assets/tiltsense-template.json'
+import {showNotification} from '@mantine/notifications';
 
 
 function App() {
@@ -84,32 +85,189 @@ function App() {
         }));
     };
 
-    const handleGenerateYAML = () => {
-        const enabledTilts = Object.values(tilts).filter(tilt => tilt.enabled);
-        console.log(enabledTilts);
-        const tiltSenseGeneratedFirmware = JSON.parse(JSON.stringify(tiltSenseFirmwareTemplate));
-        console.log(yaml.load(tiltSenseTemplateYaml));
+    const debugYAML2JSON = () => {
+        const preprocessYaml = (yamlText: string) => {
+            return yamlText
+                .replace(/!lambda\s*\|-/g, '__LAMBDA_BLOCK__')
+                .replace(/!\w+/g, '__TAG__');
+        };
 
-        if (enabledTilts.length === 1) {
-            const onBLEAdvertiseCode = `
-                 if (x.get_ibeacon().has_value()) {
-                  auto ibeacon = x.get_ibeacon().value();
-                  std::string uuid = ibeacon.get_uuid().to_string();
-                
-                  if (uuid == ${enabledTilts[0].color.id}) {
-                    float temp_c = (ibeacon.get_major() - 32) * 5.0f / 9.0f;
-                    float gravity = ibeacon.get_minor();
-                    ESP_LOGD("tilt", "[${enabledTilts[0].color.name}] Temp = %.2f °C", temp_c);
-                    id(tilt_temperature_${enabledTilts[0].color.colorKey}).publish_state(temp_c);
-                  }
-                }
-            `.trim();
-            tiltSenseGeneratedFirmware.esp32_ble_tracker.on_ble_advertise.then.push(onBLEAdvertiseCode);
+        const cleanedYaml = preprocessYaml(tiltSenseTemplateYaml);
+
+        const parsed = yaml.load(cleanedYaml);
+        console.log(parsed);
+    }
+
+    const handleGenerateYAML = () => {
+        //TODO: This is only for debug purpose, remove after
+        debugYAML2JSON();
+
+        const enabledTilts = Object.values(tilts).filter(tilt => tilt.enabled);
+        if (enabledTilts.length === 0) {
+            showNotification({
+                title: 'No Tilt selected',
+                message: 'Please select at least one Tilt before generating the YAML.',
+                color: 'red',
+                autoClose: 4000,
+                withCloseButton: true,
+            });
+            return;
         }
 
-        console.log(tiltSenseGeneratedFirmware);
-        const tiltSenseGeneratedFirmwareYAML = yaml.dump(tiltSenseGeneratedFirmware);
-        console.log(tiltSenseGeneratedFirmwareYAML);
+        //Here's where the big mess begins...
+
+        console.log(enabledTilts);
+        const tiltSenseGeneratedFirmware = JSON.parse(JSON.stringify(tiltSenseFirmwareTemplate));
+
+        //BLE Advertise Code
+        let onBLEAdvertiseCode = `
+                 if (x.get_ibeacon().has_value()) {
+      auto ibeacon = x.get_ibeacon().value();
+      std::string uuid = ibeacon.get_uuid().to_string();
+            `.trim();
+
+        enabledTilts.forEach((tilt: Tilt) => {
+            if (tilt.isPro) {
+                onBLEAdvertiseCode = `
+              ${onBLEAdvertiseCode}
+      if (uuid == ${tilt.color.id}) {
+        float temp_c = ((ibeacon.get_major() / 10.0f) - 32.0f) * 5.0f / 9.0f;
+        float gravity = ibeacon.get_minor() / 10.0f;
+        ESP_LOGD("tilt", "[${tilt.color.name}] Temperature = %.2f °C, Gravity = %.3f", temp_c, gravity);
+        id(tilt_temperature_${tilt.color.colorKey}).publish_state(temp_c);
+      }
+             `.trim();
+            } else {
+                onBLEAdvertiseCode = `
+              ${onBLEAdvertiseCode}
+      if (uuid == ${tilt.color.id}) {
+        float temp_c = (ibeacon.get_major() - 32) * 5.0f / 9.0f;
+        float gravity = ibeacon.get_minor();
+        ESP_LOGD("tilt", "[${tilt.color.name}] Temperature = %.2f °C, Gravity = %.3f", temp_c, gravity);
+        id(tilt_temperature_${tilt.color.colorKey}).publish_state(temp_c);
+      }
+             `.trim();
+            }
+
+            //Sensor management for each Tilt
+            //Gravity
+            tiltSenseGeneratedFirmware.sensor.push(
+                {
+                    "platform": "template",
+                    "id": `tilt_gravity_${tilt.color.colorKey}`,
+                    "name": `Tilt ${tilt.color.name} Gravity`,
+                    "icon": "mdi:trending-down",
+                    "accuracy_decimals": 0,
+                    "unit_of_measurement": "SG",
+                    "update_interval": "never",
+                    "on_value": {
+                        "then": [
+                            {
+                                "lvgl.label.update": {
+                                    "id": `ble_gravity_label_${tilt.color.colorKey}`,
+                                    "text": `__LAMBDA_BLOCK__
+      char buffer[8];
+      snprintf(buffer, sizeof(buffer), "%.3f", id(tilt_gravity_${tilt.color.colorKey}).state / 1000.0);
+      return std::string(buffer);`
+                                }
+                            }
+                        ]
+                    }
+                }
+            );
+            //Temperature
+            tiltSenseGeneratedFirmware.sensor.push({
+                "platform": "template",
+                "name": `Tilt ${tilt.color.name} Temperature`,
+                "id": `tilt_temperature_${tilt.color.colorKey}`,
+                "device_class": "temperature",
+                "accuracy_decimals": 1,
+                "unit_of_measurement": "°C",
+                "update_interval": "never",
+                "on_value": {
+                    "then": [
+                        {
+                            "lvgl.label.update": {
+                                "id": `ble_temp_label_${tilt.color.colorKey}`,
+                                "text": `__LAMBDA_BLOCK__ char buffer[10]; snprintf(buffer, sizeof(buffer), \"%.1f °C\", id(tilt_temperature_${tilt.color.colorKey}).state); return std::string(buffer);`
+                            }
+                        }
+                    ]
+                }
+            });
+
+            //LVGL page management for each Tilt
+            tiltSenseGeneratedFirmware.lvgl.pages.push(
+                {
+                    "id": `display_${tilt.color.colorKey}`,
+                    "widgets": [
+                        {
+                            "arc": {
+                                "id": `border_circle_${tilt.color.colorKey}`,
+                                "align": "CENTER",
+                                "arc_color": `__LAMBDA_BLOCK__
+                          if (id(enable_tilt_${tilt.color.colorKey})) { 
+                            return lv_color_hex(${tilt.color.hexColor}); 
+                          } else { 
+                            return lv_color_hex(0x808080); 
+                          }`,
+                                "arc_rounded": true,
+                                "arc_width": 20,
+                                "width": 220,
+                                "height": 220
+                            }
+                        },
+                        {
+                            "label": {
+                                "id": `ble_gravity_label_${tilt.color.colorKey}`,
+                                "align": "CENTER",
+                                "text": " ",
+                                "text_font": "montserrat_48",
+                                "y": -35
+                            }
+                        },
+                        {
+                            "label": {
+                                "id": `ble_temp_label_${tilt.color.colorKey}`,
+                                "align": "CENTER",
+                                "text": " ",
+                                "text_font": "montserrat_26",
+                                "y": 10
+                            }
+                        },
+                        {
+                            "label": {
+                                "id": `pressure_label_${tilt.color.colorKey}`,
+                                "align": "CENTER",
+                                "text": " ",
+                                "text_font": "montserrat_20",
+                                "text_color": 7368816,
+                                "y": 60
+                            }
+                        },
+                        {
+                            "label": {
+                                "align": "CENTER",
+                                "text": `Tilt ${tilt.color.name}`,
+                                "text_font": "montserrat_16",
+                                "y": 90
+                            }
+                        }
+                    ]
+                }
+            );
+        });
+
+        onBLEAdvertiseCode = `
+              ${onBLEAdvertiseCode}
+  }
+}`.trim();
+        tiltSenseGeneratedFirmware.esp32_ble_tracker.on_ble_advertise.then.push({"lambda": onBLEAdvertiseCode});
+
+        const tiltSenseGeneratedFirmwareYAML = yaml.dump(tiltSenseGeneratedFirmware).replace(
+            /^(\s*\w+):\s*\|-\n\s*__LAMBDA_BLOCK__\n/gm,
+            '$1: !lambda |-\n'
+        );
         setGeneratedYAML(tiltSenseGeneratedFirmwareYAML);
     };
 
@@ -122,6 +280,7 @@ function App() {
         a.click();
         URL.revokeObjectURL(url);
     }
+
 
     return (
         <MantineProvider>
